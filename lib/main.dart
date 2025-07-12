a92e8597c69b1aa6fe7311088529ffb6d8809aac
Dhanraj <dp914278@gmail.com>
2025-07-12 11:25:58 +0530

    Add connectivity_wrapper for network status handling
    Integrated the connectivity_wrapper package to monitor network connectivity and disable UI interaction when offline. Updated main.dart to wrap the app with ConnectivityAppWrapper and ConnectivityWidgetWrapper. Registered connectivity_plus plugins for macOS and Windows. Updated pubspec.yaml and lock file to include new dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:client/features/auth/presentation/view/splash_screen.dart';
 import 'package:client/features/settings/application/provider/theme_provider.dart';
+import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,12 +16,21 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeSettings = ref.watch(themeProvider);
 
-    return MaterialApp(
-      title: 'arr',
-      theme: FlexThemeData.light(scheme: themeSettings.flexScheme),
-      darkTheme: FlexThemeData.dark(scheme: themeSettings.flexScheme),
-      themeMode: themeSettings.themeMode,
-      home: const SplashScreen(),
+    return ConnectivityAppWrapper(
+      app: MaterialApp(
+        title: 'arr',
+        theme: FlexThemeData.light(scheme: themeSettings.flexScheme),
+        darkTheme: FlexThemeData.dark(scheme: themeSettings.flexScheme),
+        themeMode: themeSettings.themeMode,
+        home: const SplashScreen(),
+        builder: (buildContext, widget) {
+          return ConnectivityWidgetWrapper(
+            disableInteraction: true,
+            height: 70,
+            child: widget!,
+          );
+        },
+      ),
     );
   }
 }

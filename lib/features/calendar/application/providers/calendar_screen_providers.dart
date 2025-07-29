e07c93075a96ffd87654334ffa0c8ee4651c7eed
Dhanraj <dp914278@gmail.com>
2025-07-29 08:32:14 +0530

    Remove all app source files and configuration

--- lib/features/calendar/application/providers/calendar_screen_providers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final selectedDayProvider = StateProvider<DateTime>((ref) => DateTime.now());
-
-final focusedDayProvider = StateProvider<DateTime>((ref) => DateTime.now());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,30 @@
-// This file will contain actual tests for the arr client app.
-// For now, we're keeping this file as a placeholder for future tests.
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:client/main.dart';
 
 void main() {
-  // Tests will be added here in the future
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
 }
